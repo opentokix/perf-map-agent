3d49c746b697c077610c04927dd99862782e6c0b
Peter Eriksson <peter.eriksson@wigetmedia.com>
2016-10-25 10:25:06 +0200

    Adjusted script to run as root and sudo to correct user

--- bin/create-java-perf-map.sh
+++ bin/create-java-perf-map.sh
@@ -10,6 +10,8 @@ PERF_MAP_DIR=$(dirname $(readlink -f $0))/..
 ATTACH_JAR_PATH=$PERF_MAP_DIR/out/$ATTACH_JAR
 PERF_MAP_FILE=/tmp/perf-$PID.map
 
+USERNAME=$(cat /proc/${1}/status |grep Uid|cut -f 2)
+
 if [ -z "$JAVA_HOME" ]; then
   JAVA_HOME=/usr/lib/jvm/default-java
 fi
@@ -17,6 +19,6 @@ fi
 [ -d "$JAVA_HOME" ] || JAVA_HOME=/etc/alternatives/java_sdk
 [ -d "$JAVA_HOME" ] || (echo "JAVA_HOME directory at '$JAVA_HOME' does not exist." && false)
 
-sudo rm $PERF_MAP_FILE -f
-(cd $PERF_MAP_DIR/out && java -cp $ATTACH_JAR_PATH:$JAVA_HOME/lib/tools.jar net.virtualvoid.perf.AttachOnce $PID "$OPTIONS")
-sudo chown root:root $PERF_MAP_FILE
+rm $PERF_MAP_FILE -f
+(cd $PERF_MAP_DIR/out && sudo -u $USERNAME java -cp $ATTACH_JAR_PATH:$JAVA_HOME/lib/tools.jar net.virtualvoid.perf.AttachOnce $PID "$OPTIONS")
+chown root:root $PERF_MAP_FILE
